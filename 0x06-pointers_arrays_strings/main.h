3d98ef8d3195140c055ed03aff06395a55699eed
Ropafadzo <94388427+Ropafadz@users.noreply.github.com>
2022-04-24 02:57:33 +0200

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifindef _main.h_
+#define _main.h_
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +9,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+
+
+#endif
